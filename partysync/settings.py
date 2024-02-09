f71a9b4e5fb38143fa85e60c719ef266c11d0e3a
Stephen Kelly <stephen.c.kelly@gmail.com>
2024-02-09 15:35:23 -0500

    django scaffolding, config database, added psycopg2 and djangorestframework to pipfile, confirm pm runserver works

--- partysync/settings.py
+++ partysync/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'partysync'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'partysync.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'party_sync',
+        'USER': 'party_sync_admin',
+        'PASSWORD': 'password',
+        'HOST': 'localhost'
     }
 }
 
