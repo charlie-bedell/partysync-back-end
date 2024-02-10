179c89d6ec8b164b95b415ad9929fad0c67ceb63
Stephen Kelly <stephen.c.kelly@gmail.com>
2024-02-10 11:00:32 -0500

    added to models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Profile(models.Model):
+  user_id = models.OneToOneField(User),
+  
+  # stretch goal - add image here
+  def __str__(self):
+    return self.username
+
+class Party(models.Model):
+  name = models.CharField(max_length=100, required=True),
+  location = models.CharField(max_length=100, requiered=True),
+  start_time = models.DateTimeField(required=True),
+  end_time = models.DateTimeField(),
+  description = models.TextField()
+  host_id = models.ForeignKey(Profile, on_delete=models.CASCADE, default='1')
+
+  def __str__(self):
+    return self.name
+  
+class Invitation(models.Model):
+  party = models.FoerignKey(Party, on_delete=models.CASCADE, default='1')
+
